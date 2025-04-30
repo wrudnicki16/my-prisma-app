d42bdc63990291c004c89df8431e70bc8c3c85be
wrudnicki16 <wyattrudnicki@gmail.com>
2025-04-30 10:36:17 -0700

    Add posts index page

---
+++ app/posts/page.tsx
@@ -0,0 +1,24 @@
+import prisma from "@/lib/prisma";
+
+export default async function Posts() {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+    }
+  });
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Posts
+      </h1>
+      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+        {posts.map((post) => (
+          <li key={post.id} className="mb-2">
+            <h2 className="font-semibold">{post.title}</h2>
+            <p className="text-sm text-gray-600 ml-2">by {post.author.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
